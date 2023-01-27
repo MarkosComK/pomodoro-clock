65d8c91140a0cc072bfa294fa34a431984b427df
MarkosComK <markoscomks@gmail.com>
2023-01-27 15:31:33 +0000

    change the style of the button, added some icons for play and pause button, and make it responsive

--- pomodoro.js
+++ pomodoro.js
@@ -3,6 +3,9 @@ var minutes = 24
 var seconds = 60
 var clock = document.querySelector('#clock')
 
+
+var playIcon = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M3 22v-20l18 10-18 10z"/></svg>'
+var pauseIcon = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M11 22h-4v-20h4v20zm6-20h-4v20h4v-20z"/></svg>'
 //alows setInterval to get just one call
 var played = false 
 // 
@@ -14,9 +17,9 @@ function startTimer(){
     if(played == false){
         counting = setInterval(pomodoro, 1000)
         played = true
-        startBtn.innerHTML = `stop`
+        startBtn.innerHTML = pauseIcon
     } else {
-        startBtn.innerHTML = `start`
+        startBtn.innerHTML = playIcon
         stop()
         played = false
     }
@@ -29,7 +32,7 @@ function pomodoro(){
             if(breakStatus == true){
                 minutes = 24
                 stop()
-                startBtn.innerHTML = `start`
+                startBtn.innerHTML = playIcon
                 played = false
             } else {
                 stop()
