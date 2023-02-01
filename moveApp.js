dfc3d2257f78008973f926a7beb242e1ce9018f5
MarkosComK <markoscomks@gmail.com>
2023-02-01 14:48:00 +0000

    implemented the drag and move window code

--- moveApp.js
+++ moveApp.js
@@ -1,25 +1,39 @@
 var app = document.querySelector('#window')
 var topBar = document.querySelector('#top')
 
-var divWidth = 150
-var divHeight = 10
+var appX = 0 
+var appY = 0
+var mouseStartX
+var mouseStartY
+var mouseEndX
+var mouseEndY
+
 var isDragging = false
 
 topBar.addEventListener('mousedown', dragOn)
 topBar.addEventListener('mouseup', dragOff)
 topBar.addEventListener('mousemove', move)
 
-function dragOn(){
+
+function dragOn(e){
+    mouseStartX = e.clientX
+    mouseStartY = e.clientY
     isDragging = true
 }
 
-function dragOff(){
+function dragOff(e){
+    mouseEndX = e.clientX
+    mouseEndY = e.clientY
+    appX += mouseEndX - mouseStartX
+    appY += mouseEndY - mouseStartY
     isDragging = false
 }
 
 function move(e){
     if(isDragging){
-        app.style.left = `${e.clientX - divWidth}px`
-        app.style.top = `${e.clientY - divHeight}px`
+        app.style.left = `${e.clientX - (mouseStartX - appX)}px`
+        app.style.top = `${e.clientY - (mouseStartY - appY)}px`
     }
 }
+
+
