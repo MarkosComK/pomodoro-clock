96511df308e5bace52889fde47d4409740bbc58e
MarkosComK <markoscomks@gmail.com>
2023-02-01 14:54:58 +0000

    commites a new function if the user get the mouse out of the body of element

--- moveApp.js
+++ moveApp.js
@@ -1,8 +1,10 @@
 var app = document.querySelector('#window')
 var topBar = document.querySelector('#top')
-
+// these variables saves the position of the app in X/Y
 var appX = 0 
 var appY = 0
+
+// Saves the position of mouse Start and end in X/Y
 var mouseStartX
 var mouseStartY
 var mouseEndX
@@ -10,24 +12,29 @@ var mouseEndY
 
 var isDragging = false
 
-topBar.addEventListener('mousedown', dragOn)
-topBar.addEventListener('mouseup', dragOff)
+topBar.addEventListener('mousedown', dragStart)
+topBar.addEventListener('mouseup', dragEnd)
+// topBar.addEventListener('mouseout', dragOut)
 topBar.addEventListener('mousemove', move)
 
 
-function dragOn(e){
+function dragStart(e){
     mouseStartX = e.clientX
     mouseStartY = e.clientY
     isDragging = true
 }
 
-function dragOff(e){
+function dragEnd(e){
     mouseEndX = e.clientX
     mouseEndY = e.clientY
     appX += mouseEndX - mouseStartX
     appY += mouseEndY - mouseStartY
     isDragging = false
 }
+
+// function dragOut(){
+//     isDragging = false
+// }
 
 function move(e){
     if(isDragging){
