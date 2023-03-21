c3d7f92f74d550f2ccfa31881fbafe448bbeb54c
MarkosComK <markoscomks@gmail.com>
2023-03-21 19:48:10 +0000

    settings start closed

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ const settings = document.querySelector('.settings')
 const closeSettings = document.querySelector('#close-settings')
 const openSettings = document.querySelector('#open-settings')
 
+window.addEventListener('load', () => {
+    settings.classList.add('close')
+    document.querySelector('.cover').style.backgroundColor = '#F87070'
+    document.querySelector('.orange-countdown').style.backgroundColor = '#F87070'
+    document.querySelector('.orange-right').style.backgroundColor = '#F87070'
+
+})
+
 closeSettings.addEventListener('click', () => {
     settings.classList.add('close')
 })
